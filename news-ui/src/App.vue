f8f31d0afd2e8d18e0571914203b2f6661a5ac68
Kroer404 <75685007+Kroer404@users.noreply.github.com>
2024-05-31 00:34:05 +0800

    17.2.4 实现新闻列表原型设计

--- news-ui/src/App.vue
+++ news-ui/src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--<n-button>n-button</n-button>-->
+  <news-list></news-list>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+import {NButton} from 'naive-ui'
+import NewsList from "@/components/NewsList.vue";
 
-export default {
-  name: 'App',
+@Options({
   components: {
-    HelloWorld
-  }
+    NewsList,
+    NButton,
+  },
+})
+export default class App extends Vue {
 }
 </script>
 
 <style>
+a {
+  text-decoration: none;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 10px;
 }
 </style>
---
+++ news-ui/src/components/NewsList.vue
@@ -0,0 +1,41 @@
+<script lang="ts">
+import {Options, Vue} from "vue-class-component"
+import {NList, NListItem} from 'naive-ui'
+
+@Options({
+  components: {
+    components: {
+      NList,
+      NListItem,
+    }
+  },
+})
+
+export default class NewsList extends Vue {
+  private newsData: any[] = [
+    {id: "1", title: "111"},
+    {id: "2", title: "222"},
+    {id: "3", title: "333"},
+    {id: "4", title: "444"},
+    {id: "5", title: "555"},
+    {id: "6", title: "666"},
+    {id: "7", title: "777"},
+    {id: "8", title: "888"},
+  ]
+
+}
+</script>
+
+<template>
+  <n-list>
+    <n-list-item v-for="item in newsData" :key="item.title">
+      <div>
+        <a href="/">{{ item.title }}</a>
+      </div>
+    </n-list-item>
+  </n-list>
+</template>
+
+<style scoped>
+
+</style>
